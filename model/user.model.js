292a70ce77170eaa4e1b504f65def2836f405e38
Dogan <d0gan1998@live.nl>
2017-11-21 10:31:51 +0100

    .

--- model/user.model.js
+++ model/user.model.js
@@ -3,7 +3,15 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     name: String,
-    titel: String
+    title: String,
+    recipes: [{
+        name: String,
+        ingredients: [{
+            name: String,
+            weight: Number,
+            price: String
+        }]
+    }]
 }, {
     timestamps: true
 });
@@ -14,8 +22,14 @@ const User = mongoose.model('user', UserSchema);
 // Add a 'dummy' user (every time you require this file!)
 const user = new User({
     name: 'Joe',
-    titel: 'Mr.'
-
+    title: 'Mr.',
+    recipes: [
+        {name: 'Pasta bolognese', ingredients: [
+            {name: 'Tomaten', weight: 200, price: '1,50'}
+        ]},
+        {name: 'Spaghetti carbonara', ingredients: []},
+        {name: 'Pizza'}
+    ]
 }).save();
 
 module.exports = User;
